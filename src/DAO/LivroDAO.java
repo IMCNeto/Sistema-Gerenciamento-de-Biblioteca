57b9f2e12c573200bd577374aa01aad596595be6
IMCNeto <143047526+IMCNeto@users.noreply.github.com>
2023-09-15 17:08:41 -0300

    :sparkles:feat: Implementando o DAO do Livro

---
+++ src/DAO/LivroDAO.java
@@ -0,0 +1,65 @@
+package DAO;
+
+
+import model.Livro;
+
+
+import java.util.ArrayList;
+import java.util.List;
+public class LivroDAO {
+
+    private ArrayList<Livro> lista;
+    private int proxId;
+
+    public void listaOperadoresDAO(){
+        this.lista = new ArrayList<>();
+        this.proxId = 0;
+
+    }
+
+    private int getProxId(){
+        return this.proxId++;
+    }
+
+
+
+    public Livro create(Livro objeto){
+        return objeto;
+
+    }
+
+
+    public List<Livro> read(Livro objeto){
+        return this.lista;
+    }
+
+
+
+    public Livro readID(String isbn) {
+        for (Livro obj : this.lista){
+            if (obj.getISBN().equals(isbn)){
+                return obj;
+            }
+        }
+        return null;
+    }
+
+
+    public Livro update(Livro obj){
+        int index = this.lista.indexOf(obj);
+        this.lista.set(index,obj);
+        return obj;
+
+    }
+
+
+    public void delete(Livro obj){
+        this.lista.remove(obj);
+    }
+
+    public void deleteMany(){
+        this.lista.clear();
+        this.proxId = 0;
+    }
+
+}
