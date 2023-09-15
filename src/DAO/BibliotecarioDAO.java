396081d2de67130c1c2585beea7e4a928fb45afb
jhesser14 <jhessecampos1412@gmail.com>
2023-09-15 00:13:50 -0300

    :sparkles: feat: Implementando o DAO do Bibliotecario

---
+++ src/DAO/BibliotecarioDAO.java
@@ -0,0 +1,64 @@
+package DAO;
+
+import model.Bibliotecario;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class BibliotecarioDAO implements CRUD<Bibliotecario> {
+
+    private ArrayList<Bibliotecario> lista;
+    private int prox_id;
+
+    public void listaOperadoresDAO(){
+        this.lista = new ArrayList<>();
+        this.prox_id = 0;
+
+    }
+
+    private int getProx_id(){
+        return this.prox_id++;
+    }
+
+    @Override
+    public Bibliotecario create(Bibliotecario obj) {
+        obj.setId(this.prox_id);
+        this.lista.add(obj);
+        return obj;
+    }
+
+    @Override
+    public List<Bibliotecario> read(Bibliotecario obj) {
+        return this.lista;
+    }
+
+    @Override
+    public Bibliotecario readID(int id) {
+        for (Bibliotecario bibliotecario : this.lista){
+            if (bibliotecario.getId() == id){
+                return bibliotecario;
+            }
+        }
+        return null;
+    }
+
+    @Override
+    public Bibliotecario update(Bibliotecario obj) {
+        int index = this.lista.indexOf(obj);
+        this.lista.set(index,obj);
+        return obj;
+    }
+
+    @Override
+    public void delete(Bibliotecario obj) {
+        this.lista.remove(obj);
+
+    }
+
+    @Override
+    public void deleteMany() {
+        this.lista.clear();
+        this.prox_id = 0;
+
+    }
+}
