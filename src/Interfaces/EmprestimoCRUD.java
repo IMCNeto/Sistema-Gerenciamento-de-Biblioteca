37613d47c0962d97c92680c99fb5cfc3dc1e3670
IMCNeto <143047526+IMCNeto@users.noreply.github.com>
2023-09-16 17:54:14 -0300

    :sparkles:feat: Interface EmprestimoCRUD

---
+++ src/Interfaces/EmprestimoCRUD.java
@@ -0,0 +1,19 @@
+package Interfaces;
+
+import model.Emprestimo;
+import model.Usuario;
+
+import java.util.List;
+
+public interface EmprestimoCRUD extends CRUD<Emprestimo>{
+    public Emprestimo create(Emprestimo obj);
+
+    public List<Emprestimo> read(Emprestimo obj);
+
+    public Emprestimo findbyID(Usuario user);
+
+    public void delete(Emprestimo obj);
+
+    public void deleteMany();
+
+}
