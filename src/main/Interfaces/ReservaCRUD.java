6567338d11100ec55ff3e40844d23baf1a5f2573
IMCNeto <143047526+IMCNeto@users.noreply.github.com>
2023-09-19 19:38:27 -0300

    :sparkles:feat: Nova interface ReservaCRUD

---
+++ src/main/Interfaces/ReservaCRUD.java
@@ -0,0 +1,17 @@
+package main.Interfaces;
+
+import main.model.Emprestimo;
+import main.model.Livro;
+import main.model.Reserva;
+import main.model.Usuario;
+
+import java.util.List;
+
+public interface ReservaCRUD extends CRUD<Reserva> {
+    public Reserva create(Reserva obj);
+    public List<Reserva> read();
+    public int findReserva(Usuario usuario, Livro livro);
+    public void update(int index,boolean novoStatus);
+    public void delete(Reserva obj);
+    public void deleteMany();
+}
