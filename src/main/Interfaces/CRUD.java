e30c63cae8a47848b0e2ec772ea08910938788b5
jhesser14 <jhessecampos1412@gmail.com>
2023-09-21 22:45:09 -0300

    :sparkles: feat: Generics para Exceções

--- src/main/Interfaces/CRUD.java
+++ src/main/Interfaces/CRUD.java
@@ -2,13 +2,13 @@ package main.Interfaces;
 
 import java.util.List;
 
-public interface CRUD<T> {
+public interface CRUD<T, E extends Exception> {
 
     public T create(T obj);
 
     public List<T> read();
 
-    public void delete(T obj);
+    public void delete(T obj) throws E;
 
     public void deleteMany();
 }
