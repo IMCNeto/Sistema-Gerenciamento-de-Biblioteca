2a1221c4e2f7403cbba9e28193c2f3e37890ed5b
IMCNeto <143047526+IMCNeto@users.noreply.github.com>
2023-10-02 20:21:02 -0300

    :test_tube:test: novos testes do LivroDAO

--- src/test/dao/LivroDAOTest.java
+++ src/test/dao/LivroDAOTest.java
@@ -1,16 +1,26 @@
 package test.dao;
 
 import main.dao.DAO;
+import main.model.Emprestimo;
 import main.model.Livro;
+import main.model.Reserva;
+import main.model.Usuario;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.time.LocalDate;
 import java.util.ArrayList;
 import java.util.List;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+
+/**
+ * Classe responsável por realizar os testes dos métodos da classe LivroDAO
+ * @author Ilson Marinho e Jhessé Campos
+ * @version 1.0
+ */
 public class LivroDAOTest {
     Livro livro1;
     Livro livro2;
@@ -30,6 +40,9 @@ public class LivroDAOTest {
     @AfterEach
     void tearDown() throws Exception {
         DAO.getLivroDAO().deleteMany();
+        DAO.getEmprestimoDAO().deleteMany();
+        DAO.getUsuarioDAO().deleteMany();
+        DAO.getReservaDAO().deleteMany();
     }
 
 
@@ -37,6 +50,10 @@ public class LivroDAOTest {
     void criar() throws Exception {
         Livro atual = DAO.getLivroDAO().create(new Livro("Capitães de areia","Jorge Amado","D","DJC20","Conto",2001));
         List<Livro> esperado = DAO.getLivroDAO().readByAutor("Jorge Amado");
+
+        /*
+        Verificando se a lista esperada não está vazia, após criar um objeto no DAO
+         */
         assert !esperado.isEmpty();
 
     }
@@ -45,6 +62,10 @@ public class LivroDAOTest {
     void deletarObj() throws Exception {
         DAO.getLivroDAO().delete(livro4);
         int atual = DAO.getLivroDAO().read().size();
+
+        /*
+        Excluindo um objeto da lista que tinha tamanho 5, a lista deve ser 4;
+         */
         assertEquals(4,atual,"Esse teste deveria passar!");
     }
 
@@ -52,6 +73,10 @@ public class LivroDAOTest {
     void deletarTudo() throws Exception{
         DAO.getLivroDAO().deleteMany();
         int atual = DAO.getLivroDAO().read().size();
+
+        /*
+        Como chamamos a função deleteMany, o tamanho da lista deve ser 0;
+         */
         assertEquals(0,atual,"Esse teste deveria passar!");
 
     }
@@ -63,12 +88,20 @@ public class LivroDAOTest {
         livro2.setAutor("Camarguimho");
         livro2.setISBN("ZCC23");
         Livro atual = DAO.getLivroDAO().update(livro2);
+
+        /*
+        Conferindo se o objeto está sendo atualizado no DAO
+         */
         assertEquals(livro2,atual,"Esse teste deveria passar!");
     }
 
     @Test
     void lerTodaLista() throws Exception {
         int atual = DAO.getLivroDAO().read().size();
+
+        /*
+        Como adicionamos apenas 5 livros para realizar os testes, o tamanho da lista total deve ser 5;
+         */
         assertEquals(5,atual,"Esse teste deveria passar!");
     }
 
@@ -77,6 +110,10 @@ public class LivroDAOTest {
         List<Livro> atual = DAO.getLivroDAO().readByISBN("CTC20");
         List<Livro> esperado = new ArrayList<Livro>();
         esperado.add(livro3);
+
+        /*
+        conferindo se a lista atual que recebe os dados do DAO equivale a lista esperada;
+         */
         assertArrayEquals(new List[]{atual}, new List[]{esperado},"Esse Teste deveria passar");
     }
 
@@ -85,6 +122,10 @@ public class LivroDAOTest {
         List<Livro> atual = DAO.getLivroDAO().readByTitulo("cangaço");
         List<Livro> esperado = new ArrayList<Livro>();
         esperado.add(livro3);
+
+        /*
+        conferindo se a lista atual que recebe os dados do DAO equivale a lista esperada;
+         */
         assertArrayEquals(new List[]{atual}, new List[]{esperado},"Esse Teste deveria passar");
     }
 
@@ -93,6 +134,10 @@ public class LivroDAOTest {
         List<Livro> atual = DAO.getLivroDAO().readByAutor("Abreu");
         List<Livro> esperado = new ArrayList<Livro>();
         esperado.add(livro4);esperado.add(livro5);
+
+        /*
+        conferindo se a lista atual que recebe os dados do DAO equivale a lista esperada;
+         */
         assertArrayEquals(new List[]{atual}, new List[]{esperado},"Esse Teste deveria passar");
     }
 
@@ -103,7 +148,124 @@ public class LivroDAOTest {
         List<Livro> atual =  DAO.getLivroDAO().readByCategoria("Romance");
         List<Livro> esperado = new ArrayList<Livro>();
         esperado.add(livro1);esperado.add(livro4);esperado.add(livro5);
-        assertArrayEquals(new List[]{atual}, new List[]{esperado});
+
+        /*
+        conferindo se a lista atual que recebe os dados do DAO equivale a lista esperada;
+         */
+        assertEquals(atual, esperado,"Esse teste deveria passar");
+    }
+
+    @Test
+    void readMoreUsed() throws Exception {
+
+        /*
+        Dados necessários para realização dos testes;
+         */
+
+        Usuario Tiago = DAO.getUsuarioDAO().create(new Usuario("Tiago","75982830090","R. A,11,Feira VI"));
+        Usuario Pedro = DAO.getUsuarioDAO().create(new Usuario("Pedro","75982830091","R. B,14,Feira VI"));
+        Usuario Maria = DAO.getUsuarioDAO().create(new Usuario("Maria","75982830092","R. C,12,Feira VI"));
+        Emprestimo emprestimo1 = DAO.getEmprestimoDAO().create(new Emprestimo("12/09/2023",Tiago,livro1));
+
+        List<String> lista_ISBNPopulares = new ArrayList<>();
+        lista_ISBNPopulares.add(livro1.getISBN());
+
+
+        /*
+        Único livro emprestado áté aqui foi o 1 logo o ISBN deve ser a lista dos mais populares deve conter o ISBN do livro 1;
+         */
+
+        assertEquals(lista_ISBNPopulares,DAO.getLivroDAO().readMoreUsed(1),"Esse teste deveria passar");
+
+        Emprestimo emprestimo2 = DAO.getEmprestimoDAO().create(new Emprestimo("13/09/2023",Pedro,livro2));
+        DAO.getEmprestimoDAO().findbyID(1).finalizarEmprestimo(LocalDate.of(2023,9,14));
+        Emprestimo emprestimo3 = DAO.getEmprestimoDAO().create(new Emprestimo("14/09/2023",Maria,livro2));
+
+
+        /*
+        Agora o livro 2 passou a ser o livro que mais foi emprestado, logo o ISBN retornado deve ser o do livro 2;
+         */
+
+        lista_ISBNPopulares.clear();
+        lista_ISBNPopulares.add(livro2.getISBN());
+        assertEquals(lista_ISBNPopulares,DAO.getLivroDAO().readMoreUsed(1));
+
+    }
+
+
+    @Test
+    void numBorrowed() throws Exception {
+        Usuario Tiago = DAO.getUsuarioDAO().create(new Usuario("Tiago","75982830090","R. A,11,Feira VI"));
+        Emprestimo emprestimo1 = DAO.getEmprestimoDAO().create(new Emprestimo("12/09/2023",Tiago,livro1));
+        /*
+        Nesse momento foi identificado 1 livro atrasado, referente ao empréstimo 1, logo o número retornado deve ser 1;
+         */
+        int numEsperado = 1;
+        int numAtual = DAO.getLivroDAO().numBorrowed(LocalDate.of(2023,10,2));
+        assertEquals(numAtual,numEsperado,"Esse teste deveria passar");
+
+        Usuario Pedro = DAO.getUsuarioDAO().create(new Usuario("Pedro","75982830091","R. B,14,Feira VI"));
+        Emprestimo emprestimo2 = DAO.getEmprestimoDAO().create(new Emprestimo("13/09/2023",Pedro,livro2));
+
+        /*
+        Nesse momento foi identificado 2 livros atrasados, logo o número de empréstimos atrasados deve ser 2;
+         */
+        int newEsperado = 2;
+        int newAtual = DAO.getLivroDAO().numBorrowed(LocalDate.of(2023,10,1));
+        assertEquals(newAtual,newEsperado);
+
+
+    }
+
+    @Test
+    void numEmp() throws Exception {
+        Usuario Tiago = DAO.getUsuarioDAO().create(new Usuario("Tiago","75982830090","R. A,11,Feira VI"));
+        Emprestimo emprestimo1 = DAO.getEmprestimoDAO().create(new Emprestimo("12/09/2023",Tiago,livro1));
+
+        /*
+        Nesse momento existe 1 empréstimo ativo, referente ao livro 1, logo o número esperado deve ser 1;
+         */
+        int numEsperado = 1;
+        int numAtual = DAO.getLivroDAO().numEmp();
+
+        assertEquals(numEsperado,numAtual);
+
+        Usuario Pedro = DAO.getUsuarioDAO().create(new Usuario("Pedro","75982830091","R. B,14,Feira VI"));
+        Emprestimo emprestimo2 = DAO.getEmprestimoDAO().create(new Emprestimo("13/09/2023",Pedro,livro2));
+
+        /*
+        Agora existem 2 empréstimos ativos, referentes aos livros 1 e 2, logo o número esperado deve ser 2;
+         */
+        int newEsperado = 2;
+        int newAtual = DAO.getLivroDAO().numEmp();
+
+        assertEquals(newEsperado,newAtual);
+
+    }
+
+    @Test
+    void numReserved() throws Exception {
+        /*
+        Nesse momento não existem livros reservados, logo o número esperado é 0;
+         */
+        int numEsperado = 0;
+        int numAtual = DAO.getLivroDAO().numReserved();
+
+        assertEquals(numEsperado,numAtual);
+
+        Usuario Tiago = DAO.getUsuarioDAO().create(new Usuario("Tiago","75982830090","R. A,11,Feira VI"));
+        Emprestimo emprestimo1 = DAO.getEmprestimoDAO().create(new Emprestimo("12/09/2023",Tiago,livro1));
+        Usuario Pedro = DAO.getUsuarioDAO().create(new Usuario("Pedro","75982830091","R. B,14,Feira VI"));
+        Reserva reserva = DAO.getReservaDAO().create(new Reserva(Pedro,livro1));
+
+        /*
+        Agora existe 1 livro reservado, logo o número esperado é 1;
+         */
+
+        int newEsperado = 1;
+        int newAtual = DAO.getLivroDAO().numReserved();
+
+        assertEquals(newAtual,newEsperado);
     }
 
 }
