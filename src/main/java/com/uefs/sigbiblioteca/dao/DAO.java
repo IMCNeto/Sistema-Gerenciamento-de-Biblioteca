89662e05bdb5b071100fdfa902b158a69af772f9
IMCNeto <143047526+IMCNeto@users.noreply.github.com>
2023-12-10 21:07:24 -0300

    :recycle:refactor: Correção dos métodos

--- src/main/java/com/uefs/sigbiblioteca/dao/DAO.java
+++ src/main/java/com/uefs/sigbiblioteca/dao/DAO.java
@@ -8,52 +8,57 @@ package com.uefs.sigbiblioteca.dao;
  */
 
 public class DAO {
-    private static AdministradorDAO administradorDAOinstance;
-    private static BibliotecarioDAO bibliotecarioDAOinstance;
-    private static EmprestimoDAO emprestimoDAOinstance;
-    private static LivroDAO livroDAOinstance;
-    private static UsuarioDAO usuarioDAOinstance;
-    private static ReservaDAO reservaDAOinstance;
+    private static AdministradorDAODisco administradorDAOinstance;
+    private static BibliotecarioDAODisco bibliotecarioDAOinstance;
+    private static EmprestimoDAODisco emprestimoDAOinstance;
+    private static LivroDAODisco livroDAOinstance;
+    private static UsuarioDAODisco usuarioDAOinstance;
+    private static ReservaDAODisco reservaDAOinstance;
 
-    public static AdministradorDAO getAdministradorDAO() {
+    public static AdministradorDAODisco getAdministradorDAO() {
         if(administradorDAOinstance == null){
-            administradorDAOinstance = new AdministradorDAO();
+            //administradorDAOinstance = new AdministradorDAO();
+            administradorDAOinstance = new AdministradorDAODisco();
         }
         return administradorDAOinstance;
     }
 
-    public static BibliotecarioDAO getBibliotecarioDAO() {
+    public static BibliotecarioDAODisco getBibliotecarioDAO() {
         if(bibliotecarioDAOinstance == null){
-            bibliotecarioDAOinstance = new BibliotecarioDAO();
+            //bibliotecarioDAOinstance = new BibliotecarioDAO();
+            bibliotecarioDAOinstance = new BibliotecarioDAODisco();
         }
         return bibliotecarioDAOinstance;
     }
 
-    public static EmprestimoDAO getEmprestimoDAO() {
+    public static EmprestimoDAODisco getEmprestimoDAO() {
         if(emprestimoDAOinstance == null){
-            emprestimoDAOinstance = new EmprestimoDAO();
+            //emprestimoDAOinstance = new EmprestimoDAO();
+            emprestimoDAOinstance = new EmprestimoDAODisco();
         }
         return emprestimoDAOinstance;
     }
 
-    public static LivroDAO getLivroDAO() {
+    public static LivroDAODisco getLivroDAO() {
         if(livroDAOinstance == null){
-            livroDAOinstance = new LivroDAO();
-
+            //livroDAOinstance = new LivroDAO();
+            livroDAOinstance = new LivroDAODisco();
         }
         return livroDAOinstance;
     }
 
-    public static UsuarioDAO getUsuarioDAO() {
+    public static UsuarioDAODisco getUsuarioDAO() {
         if (usuarioDAOinstance == null){
-            usuarioDAOinstance = new UsuarioDAO();
+            //usuarioDAOinstance = new UsuarioDAO();
+            usuarioDAOinstance = new UsuarioDAODisco();
         }
         return usuarioDAOinstance;
     }
 
-    public static ReservaDAO getReservaDAO() {
+    public static ReservaDAODisco getReservaDAO() {
         if (reservaDAOinstance == null){
-            reservaDAOinstance = new ReservaDAO();
+            //reservaDAOinstance = new ReservaDAO();
+            reservaDAOinstance = new ReservaDAODisco();
         }
         return reservaDAOinstance;
     }
