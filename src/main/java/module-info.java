c0795dda2a1d4b43034dcc6aba13c2ef56e642bd
Ilson <ilsonetoneto06@gmail.com>
2024-02-20 12:22:54 -0300

    :sparkles:feat: add views

--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -5,4 +5,7 @@ module org.example.ssgbiblioteca {
 
     opens com.uefs.sigbiblioteca to javafx.fxml;
     exports com.uefs.sigbiblioteca;
+    exports com.uefs.sigbiblioteca.controller;
+    opens com.uefs.sigbiblioteca.controller to javafx.fxml;
+    opens com.uefs.sigbiblioteca.model;
 }
